b864d56d9aa64f0445987d6c4ae68de917df9087
Edy <edyxicon@gmail.com>
2023-04-25 21:34:02 -0600

    Add usuario in renewToken

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,9 +88,12 @@ const renewToken = async (req, res = response) => {
 
   const token = await generarJWT(uid)
 
+  const usuario = await Usuario.findById(uid)
+
   res.json({
     ok: true,
-    token
+    token,
+    usuario
   })
 }
 
